8e1807bf9736547662d7038cb209496adbc294d8
candypenguin <45968309+candypenguin@users.noreply.github.com>
2019-03-27 21:24:35 +0900

    20190327トップ画面実装

--- app/src/main/java/com/example/countryquiz/MainActivity.kt
+++ app/src/main/java/com/example/countryquiz/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.countryquiz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.StartButton)
+        startButton.setOnClickListener{
+            //遷移先に送るデータ
+            val intent  = Intent(this, Quiz::class.java)
+            startActivity(intent)
+        }
     }
 }
